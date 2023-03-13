6023b0985f25854a4b8de5677ac5ac4fb5fc5019
jamshid-zayniyev <jamshidzayniev10@gmail.com>
2023-03-13 16:11:14 +0500

    asdada
    asdada

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const posts = [
   {title:'Digital Marketing',excerpt:"ADS, Instagram, Telegram"},
 ]
 export default function Home({posts}) {
-  console.log(posts);
+  // console.log(posts);
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
@@ -17,14 +17,16 @@ export default function Home({posts}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'> 
-        <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post,index)=> <PostCard post={post.node} key={index}/> )}
-        </div>
-      </div>
       <div className='lg:col-span-4 col-span-1'>
         <div className='lg:sticky relative top-8'>
-          <PostWidget/>
-          <Categories/>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget/>
+            <Categories/>
+          </div>
+        </div>
+      </div>
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post,index)=> <PostCard post={post.node} key={index}/> )}
         </div>
       </div>
     </div>
