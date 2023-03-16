9ae08623534786d47511eeb9504d35ea3e00c459
jamshid-zayniyev <jamshidzayniev10@gmail.com>
2023-03-16 17:30:10 +0500

    ndsajdaskjd
    jndakdnaskj

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -9,7 +9,7 @@ const PostDetails = ({post}) => {
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
           <div className='col-span-1 lg:col-span-4'>
             <div className='relative lg:sticky top-8'>
-              <PostWidget/>
+              <PostWidget slug={post.slug} categories={post.categories.map(category =>category.slug)}/>
               <Categories/>
             </div>
           </div>
